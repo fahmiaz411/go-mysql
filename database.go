83b56d0318e68afcfb5bbd78d6bb37928e95a6d9
fahmiaz411 <fahmiaziz884@gmail.com>
2022-03-17 20:01:30 +0700

    change db

--- database.go
+++ database.go
@@ -7,12 +7,12 @@ import (
 
 func GetConnection() *sql.DB {
 
-	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/gosample")
+	db, err := sql.Open("mysql", "root:1234@tcp(localhost:3306)/belajar_golang?parseTime=true")
 	if err != nil {
 		panic(err)
 	}
 	
-	db.SetMaxIdleConns(10)
+	db.SetMaxIdleConns(10) // standby connection
 	db.SetMaxOpenConns(50)
 	db.SetConnMaxIdleTime(5 * time.Minute)
 	db.SetConnMaxLifetime(time.Hour)
